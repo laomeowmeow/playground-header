9f856173c0985d587a2fc1dbf0d7fd8aad87dedd
vincent <vincentnwa@me.com>
2025-03-01 12:10:08 +0800

    Add Roboto font, Material Ui icons and run code

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import { Container, Typography } from "@mui/material";
+
+export default function HomePage() {
+    return (
+        <Container
+            maxWidth="sm"
+        >
+            <Typography
+                variant="h4"
+                alignContent={"center"}
+                alignItems={"center"}
+            >Welcome to the Home Page</Typography>
+        </Container>
+    )
+}
+
