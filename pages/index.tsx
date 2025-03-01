3cd0a663d901f13587842a317729f917061b1a0a
vincent <vincentnwa@me.com>
2025-03-01 12:26:41 +0800

    Add margin

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import { Container, Typography } from "@mui/material";
+import { useState } from "react";
 
 export default function HomePage() {
+    const [headerName, setHeaderName] = useState('');
+
     return (
         <Container
             maxWidth="sm"
         >
             <Typography
                 variant="h4"
+                margin={"2rem"}
                 alignContent={"center"}
                 alignItems={"center"}
             >Welcome to the Home Page</Typography>
